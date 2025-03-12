34353a3101f0f5e12f5d4db2ea5e96b076cebb74
Nikita Antonov <nikita45454@gmail.com>
2025-03-12 18:22:56 +0500

    feat: products - добавляет таблицу

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,9 +1,44 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout class="tw-bg-gray-100" view="hhh Lpr fff">
+    <q-header>
+      <q-toolbar>
+        <q-btn flat round dense icon="menu" @click="isMini = !isMini" />
+        <q-toolbar-title>Инкубатор - торговый журнал</q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+    <q-drawer :mini="isMini" v-model="showedMenu" :width="200" :breakpoint="0">
+      <q-scroll-area class="fit">
+        <q-list padding>
+          <q-item clickable v-ripple active :to="{ name: 'home' }">
+            <q-item-section avatar>
+              <q-icon class="nav-item__icon" name="home" />
+            </q-item-section>
+            <q-item-section class="nav-item__label">Главная</q-item-section>
+          </q-item>
+          <q-item clickable v-ripple active :to="{ name: 'products.index' }">
+            <q-item-section avatar>
+              <q-icon class="nav-item__icon" name="local_mall" />
+            </q-item-section>
+            <q-item-section class="nav-item__label">Продукты</q-item-section>
+          </q-item>
+          <q-item clickable v-ripple active :to="{ to: '/2' }">
+            <q-item-section avatar>
+              <q-icon class="nav-item__icon" name="store" />
+            </q-item-section>
+            <q-item-section class="nav-item__label">Поставщики</q-item-section>
+          </q-item>
+        </q-list>
+      </q-scroll-area>
+    </q-drawer>
     <q-page-container>
       <router-view />
     </q-page-container>
   </q-layout>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { ref } from 'vue';
+
+  const showedMenu = ref(true);
+  const isMini = ref(true);
+</script>
---
+++ src/pages/ProductsPage.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-page class="page-py">
+    <div class="wrapper">
+      <h1 class="h1 tw-mb-5">Продукты</h1>
+      <div class="tw-mb-3">
+        <q-btn
+          color="primary"
+          label="Добавить"
+        />
+      </div>
+      <q-markup-table v-if="products" flat bordered>
+        <thead>
+          <tr>
+            <th class="tw-text-left">Id</th>
+            <th class="tw-text-left">Наименование</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="product in products.data"
+            :key="product.id"
+          >
+            <td>{{ product.id }}</td>
+            <td>{{ product.name }}</td>
+            <td>
+              <q-btn color="primary" round flat icon="edit" size="sm"></q-btn>
+            </td>
+          </tr>
+        </tbody>
+      </q-markup-table>
+    </div>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+  import useRepositories from 'src/composables/useRepositories';
+  import useRequest from 'src/composables/useRequest';
+
+  const api = useRepositories();
+  const { data: products } = useRequest(api.products.list);
+</script>
