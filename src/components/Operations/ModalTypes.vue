63fd8e4416d607ec7659e4675c5d126965375d17
Nikita Antonov <nikita45454@gmail.com>
2025-03-14 14:08:28 +0500

    feat: operations - встраивает модули для создания отгрузки и прихода

---
+++ src/components/Operations/ModalTypes.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-dialog v-model="value">
+    <q-card class="tw-w-full tw-bg-white">
+      <q-card-section>
+        <div class="tw-font-bold tw-text-xl tw-mb-4">Выберите тип операции</div>
+        <q-btn-group spread>
+          <q-btn class="tw-bg-green-700 tw-text-white tw-py-5" label="Деньги" icon="payments" @click="$emit('change:type', 'cash')" />
+          <q-btn class="tw-bg-blue-700 tw-text-white" label="Товары" icon="shopping_cart" @click="$emit('change:type', 'products')" />
+        </q-btn-group>
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script setup lang="ts">
+  import { TransactionType } from 'src/repositories/operations';
+
+  const value = defineModel<boolean>({ default: false });
+
+  defineEmits<{
+    (event: 'change:type', value: TransactionType): void,
+  }>();
+</script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,42 @@
 <template>
-  <q-page></q-page>
+  <q-page class="page-py">
+    <div class="wrapper">
+      <h1 class="h1 tw-mb-5">Мои опрерации</h1>
+      <div class="tw-flex tw-flex-wrap tw-gap-3 tw-max-w-[200px] tw-mb-6">
+        <q-btn class="tw-bg-orange-600 tw-text-white tw-w-full" @click="showedTypes = true; activeOperation = 'purchase'">
+          Новая отгрузка
+        </q-btn>
+        <q-btn class="tw-bg-green-600 tw-text-white tw-w-full" @click="showedTypes = true; activeOperation = 'supply'">
+          Новый приход
+        </q-btn>
+      </div>
+      <ModalTypes
+        v-model="showedTypes"
+        @change:type="activeTransaction = $event; showedTypes = false; showedCreate = true"
+      />
+      <ModalCashCreate
+        v-if="activeOperation && activeTransaction === 'cash'"
+        :operationType="activeOperation"
+        v-model="showedCreate"
+      />
+      <ModalProductsCreate
+        v-if="activeOperation && activeTransaction === 'products'"
+        :operationType="activeOperation"
+        v-model="showedCreate"
+      />
+    </div>
+  </q-page>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import ModalTypes from 'src/components/Operations/ModalTypes.vue';
+  import ModalCashCreate from 'src/components/Operations/ModalCashCreate.vue';
+  import ModalProductsCreate from 'src/components/Operations/ModalProductsCreate.vue';
+  import { OperationType, TransactionType } from 'src/repositories/operations';
+  import { ref } from 'vue';
+
+  const showedTypes = ref(false);
+  const showedCreate = ref(false);
+  const activeTransaction = ref<TransactionType | null>(null);
+  const activeOperation = ref<OperationType | null>(null);
+</script>
