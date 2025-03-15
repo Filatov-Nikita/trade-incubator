7a89417e3a687b57e060672c57e645a6f6214736
Nikita Antonov <nikita45454@gmail.com>
2025-03-15 10:54:43 +0500

    style: заменяет цвета иконок в навигации

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -8,22 +8,22 @@
     </q-header>
     <q-drawer :mini="isMini" v-model="showedMenu" :width="200" :breakpoint="0">
       <q-scroll-area class="fit">
-        <q-list padding>
-          <q-item clickable v-ripple active :to="{ name: 'home' }">
+        <q-list padding class="tw-text-gray-700">
+          <q-item exact clickable v-ripple :to="{ name: 'home' }">
             <q-item-section avatar>
-              <q-icon class="nav-item__icon" name="home" />
+              <q-icon name="home" />
             </q-item-section>
             <q-item-section class="nav-item__label">Главная</q-item-section>
           </q-item>
-          <q-item clickable v-ripple active :to="{ name: 'products.index' }">
+          <q-item exact clickable v-ripple :to="{ name: 'products.index' }">
             <q-item-section avatar>
-              <q-icon class="nav-item__icon" name="local_mall" />
+              <q-icon name="local_mall" />
             </q-item-section>
             <q-item-section class="nav-item__label">Продукты</q-item-section>
           </q-item>
-          <q-item clickable v-ripple active :to="{ name: 'companies.index' }">
+          <q-item exact clickable v-ripple :to="{ name: 'companies.index' }">
             <q-item-section avatar>
-              <q-icon class="nav-item__icon" name="store" />
+              <q-icon name="store" />
             </q-item-section>
             <q-item-section class="nav-item__label">Поставщики</q-item-section>
           </q-item>
