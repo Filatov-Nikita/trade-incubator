740b5be6c78d8b07513843fd7eca008ea45b54f6
Nikita Antonov <nikita45454@gmail.com>
2025-03-22 14:30:47 +0500

    style: files - обновляет стили для превью галереи

--- src/components/Files/PhotoGallery/Preview.vue
+++ src/components/Files/PhotoGallery/Preview.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="item">
-    <img class="img" :src="url" />
+  <div class="tw-relative">
+    <q-img
+      class="tw-w-[100px] tw-h-[100px] tw-rounded-2xl"
+      :src="url"
+    />
   </div>
 </template>
 
@@ -9,18 +12,3 @@
     url: string,
   }>();
 </script>
-
-<style scoped lang="scss">
-  .item {
-    width: 100px;
-    height: 100px;
-    border-radius: 20px;
-    overflow: hidden;
-  }
-
-  .img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
-</style>
