0be21cc276d11b2eb060218fe729b0aaf318fab7
Nikita Antonov <nikita45454@gmail.com>
2025-03-30 19:44:46 +0500

    style: layout - добавляет адаптив меню

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,11 +2,17 @@
   <q-layout class="tw-bg-gray-100" view="hhh Lpr fff">
     <q-header>
       <q-toolbar>
-        <q-btn flat round dense icon="menu" @click="isMini = !isMini" />
+        <q-btn flat round dense icon="menu" @click="showMenu" />
         <q-toolbar-title>Инкубатор - торговый журнал</q-toolbar-title>
       </q-toolbar>
     </q-header>
-    <q-drawer :mini="isMini" v-model="showedMenu" :width="200" :breakpoint="0">
+    <q-drawer
+      v-model="showedMenu"
+      :width="200"
+      :mini="isMini"
+      show-if-above
+      :breakpoint="$q.screen.sizes.sm"
+    >
       <q-scroll-area class="fit">
         <q-list padding class="tw-text-gray-700">
           <q-item exact clickable v-ripple :to="{ name: 'home' }">
@@ -38,7 +44,17 @@
 
 <script setup lang="ts">
   import { ref } from 'vue';
+  import { useQuasar } from 'quasar';
 
-  const showedMenu = ref(true);
+  const $q = useQuasar();
+
+  const showedMenu = ref(false);
   const isMini = ref(true);
+
+  function showMenu() {
+    if($q.screen.gt.sm) {
+      isMini.value = !isMini.value;
+    }
+    showedMenu.value = true;
+  }
 </script>
